4961457b88b1a972290cbb664596fc4b25ade218
Kevin Wilder <kevin.wilder@darkwolfsolutions.com>
2021-07-08 11:08:20 -0600

    test4

--- chart/tests/cypress/03-gitlab-create-project.spec.js
+++ chart/tests/cypress/03-gitlab-create-project.spec.js
@@ -13,12 +13,12 @@ describe('Create Gitlab Project', () => {
     cy.get('input[type="submit"]').click()
      
     // create project
-    cy.get('a[href="/projects/new"]').first().click()
+    cy.get('a[href="/projects/new"]').contains('New project').click()
     // cy.get('a[href="#blank_project"]').click()
     // cy.get('input[id="project_name"]').first().type('My awesome project') // for some reason, there are 2 other hidden elements with the same attributes but we only need the first one
     // cy.get('input[id="project_visibility_level_20"]').first().click()     // for some reason, there are 2 other hidden elements with the same attributes but we only need the first one
     // cy.get('input[id="project_initialize_with_readme"]').click()
     // cy.get('input[type="submit"]').first().click()                        // for some reason, there are 2 other hidden elements with the same attributes but we only need the first one
-    // cy.contains("Project 'My awesome project' was successfully created.")
+    // cy.contains('successfully created')
   })
 })
