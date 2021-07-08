cf2744ca32da6e83aa320d8f81eef0d8e77a00e2
Kevin Wilder <kevin.wilder@darkwolfsolutions.com>
2021-07-08 11:46:06 -0600

    test6

--- chart/tests/cypress/03-gitlab-create-project.spec.js
+++ chart/tests/cypress/03-gitlab-create-project.spec.js
@@ -14,7 +14,7 @@ describe('Create Gitlab Project', () => {
      
     // create project
     cy.get('a[href="/projects/new"]').eq(3).click()
-    // cy.get('a[href="#blank_project"]').click()
+    cy.get('a[href="#blank_project"]').click()
     // cy.get('input[id="project_name"]').first().type('My awesome project') // for some reason, there are 2 other hidden elements with the same attributes but we only need the first one
     // cy.get('input[id="project_visibility_level_20"]').first().click()     // for some reason, there are 2 other hidden elements with the same attributes but we only need the first one
     // cy.get('input[id="project_initialize_with_readme"]').click()
