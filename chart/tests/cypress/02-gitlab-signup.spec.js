d6411f5e7e1f156e6050c417418531e8825f760f
Kevin Wilder <kevin.wilder@darkwolfsolutions.com>
2021-07-08 09:20:07 -0600

    change user name

--- chart/tests/cypress/02-gitlab-signup.spec.js
+++ chart/tests/cypress/02-gitlab-signup.spec.js
@@ -2,12 +2,12 @@ describe('Gitlab Signup', () => {
   it('Check user is able to signup', () => {
     // test signup
     cy.visit('/users/sign_up')
-    cy.get('input[id="new_user_first_name"]').type('test')
+    cy.get('input[id="new_user_first_name"]').type('rando')
     cy.get('input[id="new_user_last_name"]').type('user')
-    cy.get('input[id="new_user_username"]').type('test.user')
-    cy.wait(5000) // wait 3 seconds for username check to complete
-    cy.get('input[id="new_user_email"]').type('test.user@example.com')
-    cy.wait(5000) // wait 3 seconds for username check to complete
+    cy.get('input[id="new_user_username"]').type('rando.user')
+    cy.wait(3000) // wait 3 seconds for username check to complete
+    cy.get('input[id="new_user_email"]').type('randouser@example.com')
+    cy.wait(3000) // wait 3 seconds for username check to complete
     cy.get('input[id="new_user_password"]').type('12345678')
     cy.get('input[type="submit"]').click()
   })
